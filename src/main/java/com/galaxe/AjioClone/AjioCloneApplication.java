1a40ad5b1a53beb095bfce90c6d47e3abab3522d
ljohnson <ljohnson@galaxe.com>
2024-03-30 18:08:39 +0530

    commented some lines in properties file

--- src/main/java/com/galaxe/AjioClone/AjioCloneApplication.java
+++ src/main/java/com/galaxe/AjioClone/AjioCloneApplication.java
@@ -3,11 +3,6 @@ package com.galaxe.AjioClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.galaxe.AjioClone.dto.ProductDetailsDto;
-import com.galaxe.AjioClone.dto.ProductDetailsDtoMapperService;
-import com.galaxe.AjioClone.dto.ProductDetailsDtoMapperServiceImpl;
 
 @SpringBootApplication
 public class AjioCloneApplication {
@@ -16,10 +11,9 @@ public class AjioCloneApplication {
 		SpringApplication.run(AjioCloneApplication.class, args);
 		System.out.println("Ajio Application Started...");
 	}
-
-	@Bean
-	public ProductDetailsDtoMapperService getProductDetailsDtoMapperService() {
-		return new ProductDetailsDtoMapperServiceImpl();
-	}
+//	@Bean
+//	public ProductDetailsDtoMapperService getProductDetailsDtoMapperService() {
+//		return new ProductDetailsDtoMapperServiceImpl();
+//	}
 	
 }
